41a6070ab924ea77c44e0a7c24c513bceeb1903f
kenterberiiskii <a.mizyakov@gmail.com>
2022-10-16 21:05:41 +0300

    lesson2 prog

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('contacts/', views.ContactsView.as_view()),
     path('docsite/', views.DocSiteView.as_view()),
     path('', views.IndexView.as_view()),
+    path('/', views.IndexView.as_view()),
     path('login/', views.LoginView.as_view()),
     path('news/', views.NewsView.as_view()),
 ]
