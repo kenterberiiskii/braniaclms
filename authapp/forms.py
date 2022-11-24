57d8c2fbb8dcab22303d4868c0c34cc1f57d42c5
kenterberiiskii <a.mizyakov@gmail.com>
2022-11-24 19:42:15 +0300

    lesson6

---
+++ authapp/forms.py
@@ -0,0 +1,61 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django import forms
+from django.core.exceptions import ValidationError
+
+
+class StyleFormMixin:
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance(field.widget, forms.widgets.CheckboxInput):
+                field.widget.attrs['class'] = 'form-check-input'
+            elif isinstance(field.widget, forms.DateTimeInput):
+                field.widget.attrs['class'] = 'form-control flatpickr-basic'
+            elif isinstance(field.widget, forms.TimeInput):
+                field.widget.attrs['class'] = 'form-control flatpickr-time'
+            elif isinstance(field.widget, forms.widgets.SelectMultiple):
+                field.widget.attrs['class'] = 'select2 form-control select2-multiple'
+            else:
+                field.widget.attrs['class'] = 'form-control'
+
+
+
+class CustomUserCreationForm(StyleFormMixin,UserCreationForm):
+
+    class Meta:
+        model = get_user_model()
+        fields = (
+            'username',
+            'email',
+            'first_name',
+            'last_name',
+            'age',
+            'avatar'
+        )
+
+    def clean_age(self):
+        age = self.cleaned_data.get('age')
+        if age < 18:
+            raise ValidationError('Молоко на губах не обсохло')
+        return age
+
+class CustomUserChangeForm(StyleFormMixin,UserChangeForm):
+
+    class Meta:
+        model = get_user_model()
+        fields = (
+            'username',
+            'email',
+            'first_name',
+            'last_name',
+            'age',
+            'avatar'
+        )
+
+    def clean_age(self):
+        age = self.cleaned_data.get('age')
+        if age < 18:
+            raise ValidationError('Молоко на губах не обсохло')
+        return age
